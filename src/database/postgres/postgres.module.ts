d4a6c4fbb2806f765f86227adb83b08e99bfb972
Michele Regina Bora <miregina@live.com>
2025-02-15 13:15:15 -0300

    chore: configure PostgreSQL database

---
+++ src/database/postgres/postgres.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { User } from 'src/resources/user/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [User],
+        synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE') || true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class PostgresModule {}
